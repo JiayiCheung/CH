91fbd8a80082d958b952f2e661cbc91ec1240d33
JiayiCheung <163282386+JiayiCheung@users.noreply.github.com>
2025-07-13 20:27:33 +0800

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .metrics import SegmentationMetrics
-from .logger import Logger
-from .visualization import Visualizer
-from .sampling_scheduler import SamplingScheduler
-
-__all__ = ['SegmentationMetrics', 'Logger', 'Visualizer', 'SamplingScheduler']
