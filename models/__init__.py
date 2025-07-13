752d431993aefe28cdcc28afbd51a9b7d6166fcb
JiayiCheung <163282386+JiayiCheung@users.noreply.github.com>
2025-07-13 20:27:13 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .vessel_segmenter import VesselSegmenter
-from .ch_branch import CHBranch
-from .spatial_branch import SpatialBranch
-from .fusion import AttentionFusion, MultiscaleFusion
-
-__all__ = [
-    'VesselSegmenter', 'CHBranch', 'SpatialBranch', 
-    'AttentionFusion', 'MultiscaleFusion'
-]
