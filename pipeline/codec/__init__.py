110a91ef3a570c4b5827de19ac94ac0a1986c50a
JiayiCheung <163282386+JiayiCheung@users.noreply.github.com>
2025-08-29 14:48:56 +0800

    Delete pipeline directory

--- pipeline/codec/__init__.py
+++
@@ -1,6 +0,0 @@
-# pipeline/codec/__init__.py
-"""编解码器模块"""
-
-from .json_codec import JSONCodec
-
-__all__ = ['JSONCodec']
--- pipeline/comm/__init__.py
+++
@@ -1,7 +0,0 @@
-
-# pipeline/comm/__init__.py
-"""通信模块"""
-
-from .channel import Channel
-
-__all__ = ['Channel']
