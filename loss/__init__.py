99cceab8ca5f2ac1e7df7177f563feb35c25e239
JiayiCheung <163282386+JiayiCheung@users.noreply.github.com>
2025-08-29 14:48:42 +0800

    Delete loss directory

--- loss/__init__.py
+++
@@ -1,9 +0,0 @@
-from .dice_loss import DiceLoss, GeneralizedDiceLoss
-from .focal_loss import FocalLoss, AdaptiveBoundaryFocalLoss
-from .combined_loss import CombinedLoss
-
-__all__ = [
-    'DiceLoss', 'GeneralizedDiceLoss',
-    'FocalLoss', 'AdaptiveBoundaryFocalLoss',
-    'CombinedLoss'
-]
